63ae3238a90569a9b7ac6375c2c87a98270d7ad3
Lorna Wilson <lw832@bath.ac.uk>
2019-09-24 14:20:25 +0000

    added empty list check

--- functions.py
+++ functions.py
@@ -3,7 +3,12 @@ def mean(list):
     Takes a list of numbers and returns the mean
     '''
     
-        total=0
-        for item in list:
-                total += item
-        return total/len(list)
+    if len(this_list) <= 0:
+        return "You passed and empty list"
+        
+        
+        
+    total=0
+    for item in list:
+        total += item
+    return total/len(list)
