06748ffc7038b2df60f342a934f64aa1b6d3b56e
Lorna Wilson <lw832@bath.ac.uk>
2019-09-24 14:37:09 +0000

    corrected typos

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
-def mean(list):
+def mean(this_list):
     '''
     Takes a list of numbers and returns the mean
     '''
     
     if len(this_list) <= 0:
-        return "You passed add empty list"
+        return "You passed an empty list"
         
         
         
     total=0
-    for item in list:
+    for item in this_list:
         total += item
-    return total/len(list)
+    return total/len(this_list)
